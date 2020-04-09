79a9ac378333ce01d32c2413d4a65013a5312aba
karin.janovjakova@asus <kjanovjakova.856@gmail.com>
2020-04-09 21:37:03 +0200

    prvy ozajsky commit

--- uloha 1.cpp
+++ uloha 1.cpp
@@ -1,11 +1,32 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 void retazec (int pocet, char * r)
 {
-	
+	int i; 
+    char pole[37]="abcdefghijklmnopqrstuvwxyz0123456789";
+    char vznik[pocet];
+    r=vznik;
+
+
+    for (i = 0; i < pocet; ++i) 
+	{
+        r[i] = pole[rand() % (sizeof(pole) - 1)];
+        printf("%c" , r[i]);
+    }
+
+    r[pocet] = 0;
 }
 
 int main() 
 {
-	
+	int i;
+	char * sm;
+	char ret[10];
+	sm=ret;
+	retazec(10,sm);
+	for (i=0;i<=10;i++)
+	{
+		printf("%c ", ret[i]);
+	}
 }
