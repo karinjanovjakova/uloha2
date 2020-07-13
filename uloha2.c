6842dcea24f1e6b0b853788462f6403e4e6fc3bb
karin.janovjakova@asus <kjanovjakova.856@gmail.com>
2020-07-13 12:59:22 +0200

    naplnenie a vypis

--- uloha2.c
+++ uloha2.c
@@ -28,3 +28,44 @@ MAT *mat_create_with_type(unsigned int rows, unsigned int cols)
 	
 	return mat;
 }
+
+void mat_random(MAT *mat)
+{
+	int i, j;
+	
+	for (i=0;i<mat->rows;i++)
+	{
+		for (j=0;j<mat->cols;j++)
+		{
+			ELEM(mat,i,j)=((float)rand()/(float)(RAND_MAX))*2.0 - 1.0;
+		}
+	}	
+}
+
+void mat_print(MAT *mat)
+{
+	int i, j;
+	
+	for (i=0;i<mat->rows;i++)
+	{
+		for (j=0;j<mat->cols;j++)
+		{
+			printf("%10.3f",ELEM(mat,i,j));
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+
+
+
+main()
+{	MAT *a;
+	unsigned int rows,cols;
+	printf("Zadaj rozmery prvej matice:\n");
+	scanf("%d %d",&rows,&cols);
+	a=mat_create_with_type(rows, cols);
+	mat_random(a);
+	mat_print(a);
+}
