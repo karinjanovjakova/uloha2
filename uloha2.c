b408d948427e1914f5471a31e61e38c4c14491f1
karin.janovjakova@asus <kjanovjakova.856@gmail.com>
2020-07-13 10:55:28 +0200

    vytvorenie

--- uloha2.c
+++ uloha2.c
@@ -9,3 +9,22 @@ typedef struct
 	unsigned int cols;
 	float *elem;
 } MAT;
+
+MAT *mat_create_with_type(unsigned int rows, unsigned int cols)
+{
+	MAT *mat=(MAT *)malloc(sizeof(MAT));
+	if (mat==NULL)
+		return NULL;
+		
+	mat->rows=rows;
+	mat->cols=cols;
+	
+	mat->elem=(float *)malloc(sizeof(float)*rows*cols);
+	if (mat->elem==NULL)
+	{
+		free(mat);
+		return NULL;
+	}
+	
+	return mat;
+}
